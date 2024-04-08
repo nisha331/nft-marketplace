13179ad5ddedf7c8dee545320c4f6dbe25704c22
Rajib Ghosh <78555404+rajibghosh9720@users.noreply.github.com>
2024-04-09 01:35:58 +0530

    Update NFTTile.js

--- src/components/NFTTile.js
+++ src/components/NFTTile.js
@@ -15,7 +15,12 @@ function NFTTile (data) {
     return (
         <Link to={newTo}>
         <div className="border-4 border-white ml-12 mt-5 mb-12 flex flex-col items-center rounded-xl w-48 md:w-72 shadow-2xl">
-            <img src={IPFSUrl} alt="" className="w-72 h-80 rounded-lg object-cover " crossOrigin="anonymous" />
+            <embed
+          src={IPFSUrl}
+          alt=""
+          className="w-full h-96 rounded-lg object-cover"
+          style={{ maxWidth: "100%", minHeight: "100%", objectFit: "cover" }}
+        />
             <div className= "text-white w-full p-2 bg-gradient-to-t from-[#4c4c4c] to-transparent rounded-lg pt-5 -mt-20">
                 <strong className="text-xl">{data.data.name}</strong>
                 <p className="display-inline">
