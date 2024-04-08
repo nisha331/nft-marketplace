623cf11d14725c7ec7e61a8b8ee82aec162eab9d
Nisha Yadav <76838660+nisha331@users.noreply.github.com>
2024-04-09 03:28:22 +0530

    Update NFTTile.js

--- src/components/NFTTile.js
+++ src/components/NFTTile.js
@@ -1,30 +1,30 @@
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import { GetIpfsUrlFromPinata } from "../utils";
 
-import {
-    BrowserRouter as Router,
-    Link,
-  } from "react-router-dom";
-  import { GetIpfsUrlFromPinata } from "../utils";
+function NFTTile(data) {
+  const newTo = {
+    pathname: "/nftPage/" + data.data.tokenId,
+  };
 
-function NFTTile (data) {
-    const newTo = {
-        pathname:"/nftPage/"+data.data.tokenId
-    }
+  const IPFSUrl = GetIpfsUrlFromPinata(data.data.image);
 
-    const IPFSUrl = GetIpfsUrlFromPinata(data.data.image);
+  return (
+    <Link to={newTo} style={{ overflow: "hidden", display: "block" }}>
+      <div className="border-4 border-white ml-12 mt-12 mb-8 flex flex-col items-center rounded-xl w-48 md:w-72 shadow-2xl" style={{ overflow: "hidden", display: "block" }}>
+        <embed
+          src={IPFSUrl}
+          alt=""
+          className="w-full h-96 rounded-lg object-cover"
+          style={{ maxWidth: "100%", minHeight: "100%", objectFit: "cover" }}
+        />
+      </div>
 
-    return (
-        <Link to={newTo}>
-        <div className="border-4 border-white ml-12 mt-5 mb-12 flex flex-col items-center rounded-xl w-48 md:w-72 shadow-2xl">
-            <img src={IPFSUrl} alt="" className="w-72 h-80 rounded-lg object-cover " crossOrigin="anonymous" />
-            <div className= "text-white w-full p-2 bg-gradient-to-t from-[#4c4c4c] to-transparent rounded-lg pt-5 -mt-20">
-                <strong className="text-xl">{data.data.name}</strong>
-                <p className="display-inline">
-                    {data.data.description}
-                </p>
-            </div>
-        </div>
-        </Link>
-    )
+      <div className="text-white w-full ml-4 mt-4" style={{ overflow: "hidden", display: "block" }}>
+        <strong className="text-xl">{data.data.name}</strong>
+        <p className="display-inline">{data.data.description}</p>
+      </div>
+    </Link>
+  );
 }
 
 export default NFTTile;
